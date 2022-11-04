ce508016c15cceb35f6632de251fce3fad25e26b
Siru <siru.eerola@helsinki.fi>
2022-11-04 15:02:04 +0200

    maksukorttitestit teht 6 ja muokattu .gitignore

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,22 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+
+    def test_ota_rahaa(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 9.00 euroa")
+
+    def test_kortin_saldo_vahenee_jos_riittavasti(self):
+        otto = self.maksukortti.ota_rahaa(200)
+        self.assertEqual(otto, True)
+
+    def test_kortin_saldo_ei_vahene_jos_ei_riittavasti(self):
+        otto = self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(otto, False)
