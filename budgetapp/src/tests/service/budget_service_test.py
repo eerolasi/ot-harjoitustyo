aca09d10e8c8340b80c48707f6c0f2fcccfb7607
Siru <siru.eerola@helsinki.fi>
2022-12-13 18:42:20 +0200

    lisätty testejä

--- budgetapp/src/tests/service/budget_service_test.py
+++ budgetapp/src/tests/service/budget_service_test.py
@@ -11,14 +11,15 @@ class TestUser(unittest.TestCase):
         user_repository.clear_table()
         transaction_repository.clear_table()
         self.user = User("user", "123", 100)
-        self.transaction = Transaction(self.user.username,"muu", 2)
+        self.transaction = Transaction(self.user.username, "muu", 2)
         self.transaction2 = Transaction(self.user.username, "ruoka", 3)
 
     def login_user(self):
-        budget_service.signup(self.user.username,self.user.password)
+        budget_service.signup(self.user.username, self.user.password)
 
     def test_signup(self):
         signed = budget_service.signup(self.user.username, self.user.password)
+
         budget_service.login(signed.username, signed.password)
         with self.assertRaises(UserExitsError):
             budget_service.signup("user", "1223")
@@ -32,25 +33,83 @@ class TestUser(unittest.TestCase):
         logged = budget_service.get_logged_user()
         self.assertEqual(logged.username, self.user.username)
 
+    def test_logout(self):
+        login = budget_service.signup("user4", "1224")
+        logout = budget_service.logout()
+        self.assertNotEqual(login, logout)
+
     def test_add_budget(self):
         self.login_user()
         with self.assertRaises(InvalidInputError):
             budget_service.add_budget(-1)
+        with self.assertRaises(InvalidInputError):
+            budget_service.add_budget("hei")
         budget = budget_service.add_budget(100)
         self.assertEqual(100, budget)
 
-    def test_add_transaction(self):
+    def test_add_income(self):
         self.login_user()
+        budget_service.add_budget(self.user.budget)
+
+        with self.assertRaises(InvalidInputError):
+            budget_service.add_income(self.user.budget, "moi")
+        budget = budget_service.add_income(self.user.budget, 2)
+        self.assertEqual(budget, 102)
+
+    def test_add_transaction(self):
         with self.assertRaises(InvalidInputError):
             budget_service.add_transaction("muu", -1)
+        with self.assertRaises(InvalidInputError):
+            budget_service.add_transaction("sijoitukset", "lol")
         transaction = budget_service.add_transaction(
             self.transaction2.category, self.transaction2.amount)
         self.assertEqual(3, transaction)
 
+    def test_get_budget(self):
+        self.login_user()
+        budget = budget_service.get_budget()
+        self.assertEqual(budget, 0)
+        budget_service.add_budget(int(self.user.budget))
+        budget2 = budget_service.get_budget()
+        self.assertEqual(budget2, 100)
+
     def test_get_transactions_sum(self):
-        budget_service.get_transactions_sum()
+        transactions = budget_service.get_transactions_sum()
+        self.assertEqual(transactions, 0)
         budget_service.add_transaction(
             self.transaction.category, self.transaction2.amount)
-        budget_service.add_transaction( "muu", 5)
+        budget_service.add_transaction("muu", 5)
         sum = budget_service.get_transactions_sum()
         self.assertEqual(sum, 8)
+
+    def test_get_transactions_by_category(self):
+        budget_service.add_transaction(
+            self.transaction.category, self.transaction.amount)
+        category = budget_service.get_transactions_by_category()
+        self.assertEqual(
+            category, {self.transaction.category: self.transaction.amount})
+
+    def test_get_balance(self):
+        self.login_user()
+        balance = budget_service.get_balance()
+        self.assertEqual(balance, 0)
+
+        budget_service.add_transaction(
+            self.transaction.category, self.transaction.amount)
+        balance = budget_service.get_balance()
+        self.assertEqual(balance, -2)
+
+        budget_service.clear_all()
+        budget_service.add_budget(self.user.budget)
+        balance = budget_service.get_balance()
+        self.assertEqual(balance, 100)
+
+        budget_service.add_transaction(
+            self.transaction.category, self.transaction.amount)
+        budget_service.add_budget(self.user.budget)
+        balance = budget_service.get_balance()
+        self.assertEqual(balance, 98)
+
+    def test_clear_all(self):
+        clear = budget_service.clear_all()
+        self.assertEqual(clear, self.user.username)
